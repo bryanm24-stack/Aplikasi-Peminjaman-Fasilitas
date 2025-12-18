f614e4469098494e13b87a4f20fde51e4be27fc1
kennethrobertie <kennethrobertie77@gmail.com>
2025-12-18 20:53:16 +0700

    Perbaikan error
    ..

--- Projek Pv-Uas/src/preload/index.js
+++ Projek Pv-Uas/src/preload/index.js
@@ -1,7 +1,6 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
 const api = {
   getUsers: () => ipcRenderer.invoke('get_users'),
   saveUsers: (data) => ipcRenderer.invoke('save_users', data),
@@ -16,9 +15,6 @@ const api = {
   saveFeedbacks: (data) => ipcRenderer.invoke('save_feedbacks', data),
 }
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
